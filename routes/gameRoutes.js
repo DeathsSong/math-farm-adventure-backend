e8c56b15413dde3b2f232e524f405d8cd7823983
DeathsSong <michelle.sherrie@yahoo.com>
2024-01-27 02:46:50 -0800

    AddedGET route for games and POST for barnComplete

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -14,4 +14,46 @@ router.post('/game', async (req, res) => {
   res.json (newGame)
 });
 
+
+
+// Retrieve the game status
+router.get('/game', async (req, res) => {
+  try {
+    const gameStatus = await Game.findOne();
+
+    if (gameStatus) {
+      res.json({
+        barnComplete: gameStatus.barnComplete,
+        cropComplete: gameStatus.cropComplete,
+        chickenComplete: gameStatus.chickenComplete,
+        penComplete: gameStatus.penComplete,
+      });
+    } else {
+      res.status(404).json({ success: false, message: 'Game not found' });
+    }
+  } catch (error) {
+    console.error('Failed to fetch game status:', error.message);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+
+
+
+//Try to update barnComplete to true
+router.post('/barnComplete', async (req, res) => {
+  console.log('You have reached the barnComplete route');
+
+  try {
+    await Game.findOneAndUpdate(
+      { barnComplete: true },
+      // { new: true }
+    );
+    res.json({ success: true, message: 'Barn status updated successfully' });
+  } catch (error) {
+    console.error('Failed to update barnComplete:', error.message);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
 module.exports = router;
